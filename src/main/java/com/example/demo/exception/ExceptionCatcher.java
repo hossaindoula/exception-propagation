9d026e6a5d951bd7038ea15b01981d3e373eef39
Mohammed Hossain Doula <hossaindoula@gmail.com>
2017-12-11 17:06:08 +0600

    ExceptionCatcher
    ExceptionCatcher

--- src/main/java/com/example/demo/exception/ExceptionCatcher.java
+++ src/main/java/com/example/demo/exception/ExceptionCatcher.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
+import com.google.common.collect.ImmutableMap;
 
 /**
  * Created by doula_itc on 2017-06-07.
@@ -16,12 +15,10 @@ import java.util.Map;
 @ControllerAdvice
 public class ExceptionCatcher extends ResponseEntityExceptionHandler {
 
-
     @ExceptionHandler({ ApiException.class })
     public ResponseEntity<Object> handleBadRequest(final ApiException ex, final WebRequest request) {
-        Map<String, Object> bodyOfResponse = new LinkedHashMap<>();
-        bodyOfResponse.put("error_status", ex.getServiceStatus());
-        bodyOfResponse.put("error_message", ex.getMessage());
-        return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), ex.getServiceStatus(), request);
+        return handleExceptionInternal(ex, 
+                                       ImmutableMap.of("error_status", ex.getServiceStatus(), "error_message", ex.getMessage()), 
+                                       new HttpHeaders(), ex.getServiceStatus(), request);
     }
 }
